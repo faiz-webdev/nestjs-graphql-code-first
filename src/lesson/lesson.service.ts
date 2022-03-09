6c1a697e29cb19d7e448b16e049f069106731f79
faijuddin <faijuddin@intelegencia.com>
2022-03-09 14:08:50 +0530

    initial commit

---
+++ src/lesson/lesson.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Lesson } from './lesson.entity';
+import { Repository } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+@Injectable()
+export class LessonService {
+  constructor(
+    @InjectRepository(Lesson) private lessonRepository: Repository<Lesson>,
+  ) {}
+
+  async createLesson(name, startDate, endDate): Promise<Lesson> {
+    const lesson = this.lessonRepository.create({
+      id: uuid(),
+      name,
+      startDate,
+      endDate,
+    });
+
+    return this.lessonRepository.save(lesson);
+  }
+
+  async getLesson(id: string): Promise<Lesson> {
+    const lesson = this.lessonRepository.findOne({
+      id,
+    });
+
+    return lesson;
+  }
+
+  async lessons(): Promise<Lesson[]> {
+    const lesson = this.lessonRepository.find({});
+    return lesson;
+  }
+}
